e154d772fc59717c55e11a009f48c19a4771a5f5
ird <ird87.post.ru@gmail.com>
2019-03-31 19:34:19 +0300

    +

--- ModulWIFI.py
+++ ModulWIFI.py
@@ -8,26 +8,26 @@ class WIFI(object):
 
 
     def wifiscan(self):
-       allSSID = Cell.all('wlan0')
-       allSSID_list = list(allSSID)
-       print(allSSID_list)   # prints all available WIFI SSIDs
-       myssid= 'Cell(ssid = vivekHome)'   # vivekHome is my wifi name
-       myssidA = None
-       for i in range(len(allSSID_list)):
+        allSSID = Cell.all('wlan0')
+        allSSID_list = list(allSSID)
+        print(allSSID_list)   # prints all available WIFI SSIDs
+        myssid= 'Cell(ssid = vivekHome)'   # vivekHome is my wifi name
+        myssidA = None
+        for i in range(len(allSSID_list)):
             if str(allSSID_list[i]) == myssid:
-                    a = i
-                    myssidA = allSSID_list[a]
-                    print("myssidA: " + myssidA)
-                    break
+                a = i
+                myssidA = allSSID_list[a]
+                print("myssidA: " + myssidA)
+                break
             else:
-                    print("getout")
+                print("getout")
 
-       # Creating Scheme with my SSID.
-       myssid= Scheme.for_cell('wlan0', 'home', myssidA, 'vivek1234')   # vive1234 is the password to my wifi myssidA is the wifi name
+        # Creating Scheme with my SSID.
+        myssid = Scheme.for_cell('wlan0', 'home', myssidA, 'vivek1234')   # vive1234 is the password to my wifi myssidA is the wifi name
 
-       print(myssid)
-       myssid.save()
-       myssid.activate()
+        print(myssid)
+        myssid.save()
+        myssid.activate()
 
 
 
