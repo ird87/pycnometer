d31acc4ca4e4f05875c82d050728b5dec4fe9653
ird <ird87.post.ru@gmail.com>
2019-03-31 19:40:56 +0300

    +

--- ModulWIFI.py
+++ ModulWIFI.py
@@ -11,7 +11,7 @@ class WIFI(object):
         allSSID = Cell.all('wlan0')
         allSSID_list = list(allSSID)
         # print(allSSID_list)   # prints all available WIFI SSIDs
-        myssid= 'Cell(ssid = {0})'.format(self.wifi_name)   # vivekHome is my wifi name
+        myssid= 'Cell(ssid={0})'.format(self.wifi_name)   # vivekHome is my wifi name
         print("myssid: " + myssid)
         myssidA = None
         for i in range(len(allSSID_list)):
