11935f5d6804fd44701d2846d1edce22c2dacf4d
ird <ird87.post.ru@gmail.com>
2019-03-31 19:36:31 +0300

    +

--- ModulWIFI.py
+++ ModulWIFI.py
@@ -3,15 +3,15 @@ from wifi import Cell, Scheme
 class WIFI(object):
     """Конструктор класса. Поля класса"""
     def __init__(self):
-        self.wifi_name = ""
-        self.wifi_pass = ""
+        self.wifi_name = "Gleeson55"
+        self.wifi_pass = "Brendan19"
 
 
     def wifiscan(self):
         allSSID = Cell.all('wlan0')
         allSSID_list = list(allSSID)
         print(allSSID_list)   # prints all available WIFI SSIDs
-        myssid= 'Cell(ssid = vivekHome)'   # vivekHome is my wifi name
+        myssid= 'Cell(ssid = {0})'.format(self.wifi_name)   # vivekHome is my wifi name
         myssidA = None
         for i in range(len(allSSID_list)):
             if str(allSSID_list[i]) == myssid:
@@ -23,7 +23,7 @@ class WIFI(object):
                 print("getout")
 
         # Creating Scheme with my SSID.
-        myssid = Scheme.for_cell('wlan0', 'home', myssidA, 'vivek1234')   # vive1234 is the password to my wifi myssidA is the wifi name
+        myssid = Scheme.for_cell('wlan0', 'home', myssidA, self.wifi_pass)   # vive1234 is the password to my wifi myssidA is the wifi name
 
         print(myssid)
         myssid.save()
